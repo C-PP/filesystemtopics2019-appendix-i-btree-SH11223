e9d6c54877b3f5dff41eea98d50325aef66b395f
SH11223 <ight13751@naver.com>
2019-05-28 13:51:55 +0900

    Update testbtree.cpp

--- testbtree.cpp
+++ testbtree.cpp
@@ -7,41 +7,7 @@
 #include "btree.h"
 using namespace std;
 /*
-final tree depth 3 ( root - 1-2-3) but in code depth 2
-in result b-tree inserting V == height 2
-b-tree always balanced
-수요일까지...! 그리고 다음주 목요일이 마지막 클래스 hash!!!
 
-
-correct insertion!! 
-deletion 내맘대로...!!
-btree - remove
-leaf node / non leaf node
-non leaf node- separator, append to root
-
-
-leaf node?? redistribute merge
-
-
-	int InOrdertraversal(); !!! sorted result?
-			10
-		/		\
-	5				20
-				/		\
-			15				30
-5-10-15-20-30 순서로
-left - itself - right 출력
-
-level ordering?? root - child - child ... - leaf 이용
-queue 사용 
-root - c1 - c2
-root - c1 - c2 (pop) - c11 -c12 -c21 -c22
-...
-
-
-
-until! 
-s
 */
 const char * keys = "CSDTAMPIBWN GURKEHOLJYQZFXV";
 
